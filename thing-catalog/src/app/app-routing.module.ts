94b8dc38a12ab39d10f9ceb7876bf1b68f3eecce
MeltyPlayer <mastergamepro@hotmail.com>
2018-12-02 15:20:07 -0500

    Created placeholder thing page.

--- thing-catalog/src/app/app-routing.module.ts
+++ thing-catalog/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ThingComponent }      from './thing/thing.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'thing', component: ThingComponent,
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
