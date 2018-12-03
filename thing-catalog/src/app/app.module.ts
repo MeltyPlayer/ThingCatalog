7822c861a70689c678ab62b44d0e216697793633
MeltyPlayer <mastergamepro@hotmail.com>
2018-12-02 23:25:00 -0500

    Created appbar.

--- thing-catalog/src/app/app.module.ts
+++ thing-catalog/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ThingComponent } from './thing/thing.component';
+import { AppbarComponent } from './common/appbar/appbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ThingComponent
+    ThingComponent,
+    AppbarComponent
   ],
   imports: [
     BrowserModule,
--- thing-catalog/src/app/thing/thing.component.ts
+++ thing-catalog/src/app/thing/thing.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-thing',
+  selector: 'thing',
   templateUrl: './thing.component.html',
   styleUrls: ['./thing.component.scss']
 })
